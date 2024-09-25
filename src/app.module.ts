bf39ec26c90cdef408199cf01f5737e3d1013446
PAVANKUMARKB1996 <pavankumar.kb@casttree.com>
2024-09-26 00:40:05 +0530

    mongodb configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        console.log("db url", config.get("DB_URL"));
+        return {
+          uri: config.get("DB_URL"),
+        };
+      },
+      inject: [ConfigService],
+    }),],
+  
   controllers: [AppController],
   providers: [AppService],
 })
